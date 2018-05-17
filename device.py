b77caebbc9a5e3a5e4a71fce11a995fa1a9a870b
Peter Czimmermann <peter.czimmermann@kantar.com>
2018-05-17 10:40:52 -0700

    Skeleton IoT code

---
+++ device.py
@@ -0,0 +1,90 @@
+from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTClient
+from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTShadowClient
+import logging
+import time
+import json
+
+device_seq = "6"
+
+host = "a3mfkf3z93nqt8.iot.us-west-2.amazonaws.com"
+rootCAPath = "certs/root-CA.crt"
+certificatePath = "certs/team55device%s.cert.pem" % device_seq
+privateKeyPath = "certs/team55device%s.private.key" % device_seq
+clientId = "team55"
+
+thingName = "team55device%s" % device_seq
+
+# logging
+logger = logging.getLogger("AWSIoTPythonSDK.core")
+logger.setLevel(logging.INFO)
+streamHandler = logging.StreamHandler()
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+streamHandler.setFormatter(formatter)
+logger.addHandler(streamHandler)
+
+# the client
+myAWSIoTMQTTClient = AWSIoTMQTTClient(clientId)
+myAWSIoTMQTTClient.configureEndpoint(host, 8883)
+myAWSIoTMQTTClient.configureCredentials(rootCAPath, privateKeyPath, certificatePath)
+
+myAWSIoTMQTTClient.configureAutoReconnectBackoffTime(1, 32, 20)
+myAWSIoTMQTTClient.configureOfflinePublishQueueing(-1)  # Infinite offline Publish queueing
+myAWSIoTMQTTClient.configureDrainingFrequency(2)  # Draining: 2 Hz
+myAWSIoTMQTTClient.configureConnectDisconnectTimeout(10)  # 10 sec
+myAWSIoTMQTTClient.configureMQTTOperationTimeout(5)  # 5 sec
+
+# Init AWSIoTMQTTShadowClient
+myAWSIoTMQTTShadowClient = AWSIoTMQTTShadowClient(clientId)
+myAWSIoTMQTTShadowClient.configureEndpoint(host, 8883)
+myAWSIoTMQTTShadowClient.configureCredentials(rootCAPath, privateKeyPath, certificatePath)
+
+# AWSIoTMQTTShadowClient configuration
+myAWSIoTMQTTShadowClient.configureAutoReconnectBackoffTime(1, 32, 20)
+myAWSIoTMQTTShadowClient.configureConnectDisconnectTimeout(10)  # 10 sec
+myAWSIoTMQTTShadowClient.configureMQTTOperationTimeout(5)  # 5 sec
+
+myAWSIoTMQTTShadowClient.connect()
+
+deviceState = {}
+
+
+def customShadowCallback_Delta(payload, responseStatus, token):
+    # payload is a JSON string ready to be parsed using json.loads(...)
+    # in both Py2.x and Py3.x
+    print(responseStatus)
+    payloadDict = json.loads(payload)
+    print("++++++++DELTA++++++++++")
+    print("state: " + str(payloadDict["state"]))
+    print("version: " + str(payloadDict["version"]))
+    print("+++++++++++++++++++++++\n\n")
+    deviceState.update(payloadDict["state"])
+
+
+def customShadowCallback_Get(payload, responseStatus, token):
+    # payload is a JSON string ready to be parsed using json.loads(...)
+    # in both Py2.x and Py3.x
+    print(responseStatus)
+    payloadDict = json.loads(payload)
+    print("++++++++GET++++++++++")
+    print("state: " + str(payloadDict["state"]))
+    print("version: " + str(payloadDict["version"]))
+    print("+++++++++++++++++++++++\n\n")
+    deviceState.update(payloadDict["state"])
+
+
+# Create a deviceShadow with persistent subscription
+deviceShadowHandler = myAWSIoTMQTTShadowClient.createShadowHandlerWithName(thingName, True)
+
+# Listen on deltas
+deviceShadowHandler.shadowRegisterDeltaCallback(customShadowCallback_Delta)
+
+deviceShadowHandler.shadowGet(customShadowCallback_Get, 5)
+
+topic = "saturn"
+message = "test"
+messageJson = json.dumps({'message': message, 'device': device_seq})
+myAWSIoTMQTTClient.publish(topic, messageJson, 1)
+
+# Loop forever to react to device state delta requests
+while True:
+    time.sleep(1)
---
+++ server.py
@@ -0,0 +1,78 @@
+from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTClient
+from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTShadowClient
+import logging
+import time
+import json
+
+device_seq = "6"
+
+
+def updateDeviceState(device_seq, device_state):
+    host = "a3mfkf3z93nqt8.iot.us-west-2.amazonaws.com"
+    rootCAPath = "certs/root-CA.crt"
+    certificatePath = "certs/team55device%s.cert.pem" % device_seq
+    privateKeyPath = "certs/team55device%s.private.key" % device_seq
+    clientId = "team55"
+
+    thingName = "team55device%s" % device_seq
+
+    # Custom Shadow callback
+    def customShadowCallback_Update(payload, responseStatus, token):
+        # payload is a JSON string ready to be parsed using json.loads(...)
+        # in both Py2.x and Py3.x
+        if responseStatus == "timeout":
+            print("Update request " + token + " time out!")
+        if responseStatus == "accepted":
+            payloadDict = json.loads(payload)
+            print("~~~~~~~~~~~~~~~~~~~~~~~")
+            print("Update request with token: " + token + " accepted!")
+            print("property: " + str(payloadDict["state"]["desired"]["property"]))
+            print("~~~~~~~~~~~~~~~~~~~~~~~\n\n")
+        if responseStatus == "rejected":
+            print("Update request " + token + " rejected!")
+
+    def customShadowCallback_Delete(payload, responseStatus, token):
+        if responseStatus == "timeout":
+            print("Delete request " + token + " time out!")
+        if responseStatus == "accepted":
+            print("~~~~~~~~~~~~~~~~~~~~~~~")
+            print("Delete request with token: " + token + " accepted!")
+            print("~~~~~~~~~~~~~~~~~~~~~~~\n\n")
+        if responseStatus == "rejected":
+            print("Delete request " + token + " rejected!")
+
+    # logging
+    logger = logging.getLogger("AWSIoTPythonSDK.core")
+    logger.setLevel(logging.INFO)
+    streamHandler = logging.StreamHandler()
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    streamHandler.setFormatter(formatter)
+    logger.addHandler(streamHandler)
+
+    # Init AWSIoTMQTTShadowClient
+    myAWSIoTMQTTShadowClient = AWSIoTMQTTShadowClient(clientId)
+    myAWSIoTMQTTShadowClient.configureEndpoint(host, 8883)
+    myAWSIoTMQTTShadowClient.configureCredentials(rootCAPath, privateKeyPath, certificatePath)
+
+    # AWSIoTMQTTShadowClient configuration
+    myAWSIoTMQTTShadowClient.configureAutoReconnectBackoffTime(1, 32, 20)
+    myAWSIoTMQTTShadowClient.configureConnectDisconnectTimeout(10)  # 10 sec
+    myAWSIoTMQTTShadowClient.configureMQTTOperationTimeout(5)  # 5 sec
+
+    myAWSIoTMQTTShadowClient.connect()
+
+    # Create a deviceShadow with persistent subscription
+    deviceShadowHandler = myAWSIoTMQTTShadowClient.createShadowHandlerWithName(thingName, True)
+
+    # Delete shadow JSON doc
+    deviceShadowHandler.shadowDelete(customShadowCallback_Delete, 5)
+
+    deviceShadowHandler.shadowUpdate(json.dumps({"state": {"desired": device_state}}), customShadowCallback_Update, 5)
+
+
+updateDeviceState(1, {"range": [0, 1000000]})
+updateDeviceState(2, {"range": [0, 1000000]})
+updateDeviceState(3, {"range": [0, 1000000]})
+updateDeviceState(4, {"range": [0, 1000000]})
+updateDeviceState(5, {"range": [0, 1000000]})
+updateDeviceState(6, {"range": [0, 1000000]})
