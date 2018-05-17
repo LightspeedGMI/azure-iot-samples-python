e7eb7f2a01414d8ba0e2e9551fff7c4a4191d2db
Kevin Zhou <zhoukang@gmail.com>
2018-05-17 11:57:51 -0700

    Move to app/server

--- app/server/test/__init__.py
+++ app/server/test/__init__.py
@@ -1,4 +1,4 @@
 import os
 import sys
 here = os.path.dirname(os.path.realpath(__file__))
-sys.path.append(os.path.join(here, "../vendored"))
+sys.path.append(os.path.join(here, "../../../vendored"))
