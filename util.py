181b74f95c5508b03f514ba570263310a48905ef
Kevin Zhou <zhoukang@gmail.com>
2018-05-17 09:34:10 -0700

    skeleton of util function

---
+++ util.py
@@ -0,0 +1,16 @@
+
+
+distribution = [101, 121, 131, 20, 24, 29, 123, 99]
+
+
+def median_bucket(distribution):
+    """Determine which bucket in the distribution contain the median."""
+    # TODO implementati101, 121, 131, .... , 123, 99on
+    head = 0
+    tail = len(distribution)
+    sum_head = 0
+    sum_tail = 0
+    while (tail - head < 2):
+        if 
+
+    return 3
